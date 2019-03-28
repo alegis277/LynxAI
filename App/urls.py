b39d435b18e6af80b8eef6724e9cf470bf86c270
Oscar Andrés <oscarandres@Oscars-Air-32.uniandes.edu.co>
2019-03-28 14:44:14 -0500

    f_comm

---
+++ App/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls import include, re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.png', permanent=True)
+
+from . import views
+
+urlpatterns = [
+	path('favicon.ico', favicon_view, name='favicon'),
+	path('', views.index, name='index'),
+]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn LynxAI.wsgi --log-file -
