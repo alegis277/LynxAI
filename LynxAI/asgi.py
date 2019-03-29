18e3cd7fae56eb23d51aa4203a9be831f1ed95f7
Oscar Andrés <oscarandres@Oscars-Air-32.uniandes.edu.co>
2019-03-28 20:59:59 -0500

    Automatic Commit

--- LynxAI/asgi.py
+++ LynxAI/asgi.py
@@ -6,6 +6,7 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
